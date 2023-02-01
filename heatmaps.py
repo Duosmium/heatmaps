481f5b14f21824a05e88e94e4bb5bea8243d71a7
Tomi Chen <tctree333@users.noreply.github.com>
2023-01-31 16:34:43 -0800

    fix heatmaps

--- heatmaps.py
+++ heatmaps.py
@@ -1,23 +1,20 @@
-import requests
 import sys
+
+import matplotlib.pyplot as plt
 import pandas
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 pandas.options.mode.chained_assignment = None
 
 tournament: str = ""
 try:
-    tournament: str = sys.argv[1].split("/")[-1]
+    tournament = sys.argv[1].split("/")[-1]
 except IndexError:
     print("No tournament (e.g. 2019-06-01_nationals_c) provided!")
     exit(1)
-r = requests.get(f"https://duosmium.org/results/{tournament}")
-comp_yaml = requests.get(f"https://duosmium.org/data/{tournament}.yaml")
-p = pandas.read_html(r.text)
-table = p[0]
-table = table[table.columns[4:-1]]
-table.columns = [c.replace("  Td", "").replace("  T", "") for c in table.columns]
-corr = table.corr()
+
+table = pandas.read_csv(f"https://duosmium.org/results/csv/{tournament}/")
+table = table[table.columns[8:]]
+corr = table.corr(numeric_only=True)
 plt.figure(figsize=(20, 20))
-sns.heatmap(data=corr, annot=True, cmap="YlGnBu").get_figure().savefig(f"{tournament}_corr.png")
+sns.heatmap(data=corr, annot=True, cmap="YlGnBu").get_figure().savefig(f"corr.png")
